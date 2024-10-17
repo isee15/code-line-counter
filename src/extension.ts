38599c6442738d84f040d06aa708ecc32673b40f
isee15 <isee15@outlook.com>
2024-10-17 13:18:45 +0800

    0.0.3

--- src/extension.ts
+++ src/extension.ts
@@ -8,12 +8,398 @@ const globAsync = promisify(glob);
 
 // 定义语言和文件扩展名的映射
 const defaultLanguageExtensions: { [key: string]: string[] } = {
-  JavaScript: [".js", ".jsx"],
-  TypeScript: [".ts", ".tsx"],
-  Python: [".py"],
+  // JavaScript: [".js", ".jsx"],
+  ABAP: [".abap"],
+  ActionScript: [".as"],
+  Ada: [".ada", ".adb", ".ads", ".pad"],
+  "ADSO/IDSM": [".adso"],
+  Agda: [".agda", ".lagda"],
+  AMPLE: [".ample", ".dofile", ".startup"],
+  AnsProlog: [".lp"],
+  Ant: [".build.xml"],
+  "ANTLR Grammar": [".g", ".g4"],
+  "Apex Class": [".cls"],
+  "Apex Trigger": [".trigger"],
+  APL: [
+    ".apl",
+    ".apla",
+    ".aplc",
+    ".aplf",
+    ".apli",
+    ".apln",
+    ".aplo",
+    ".dyalog",
+    ".dyapp",
+    ".mipage",
+  ],
+  AppleScript: [".applescript"],
+  "Arduino Sketch": [".ino"],
+  ArkTs: [".ets"],
+  Arturo: [".art"],
+  AsciiDoc: [".adoc", ".asciidoc"],
+  ASP: [".asa", ".ashx", ".asp", ".axd"],
+  "ASP.NET": [
+    ".asax",
+    ".ascx",
+    ".asmx",
+    ".aspx",
+    ".master",
+    ".sitemap",
+    ".webinfo",
+  ],
+  AspectJ: [".aj"],
+  Assembly: [".a51", ".asm", ".nasm", ".S", ".s"],
+  Astro: [".astro"],
+  Asymptote: [".asy"],
+  AutoHotkey: [".ahk", ".ahkl"],
+  awk: [".auk", ".awk", ".gawk", ".mawk", ".nawk"],
+  Bazel: [".BUILD"],
+  "BizTalk Orchestration": [".odx"],
+  "BizTalk Pipeline": [".btp"],
+  Blade: [".blade", ".blade.php"],
+  "Bourne Again Shell": [".bash"],
+  "Bourne Shell": [".sh"],
+  BrightScript: [".brs"],
+  builder: [".xml.builder"],
+  C: [".c", ".cats", ".ec", ".idc", ".pgc"],
+  "C Shell": [".csh", ".tcsh"],
+  "C#": [".cs"],
+  "C# Designer": [".designer.cs"],
+  "C++": [
+    ".C",
+    ".c++",
+    ".c++m",
+    ".cc",
+    ".ccm",
+    ".CPP",
+    ".cpp",
+    ".cppm",
+    ".cxx",
+    ".cxxm",
+    ".h++",
+    ".inl",
+    ".ipp",
+    ".ixx",
+    ".pcc",
+    ".tcc",
+    ".tpp",
+  ],
+  "C/C++ Header": [".H", ".h", ".hh", ".hpp", ".hxx"],
+  Cairo: [".cairo"],
+  "Cake Build Script": [".cake"],
+  Carbon: [".carbon"],
+  CCS: [".ccs"],
+  Chapel: [".chpl"],
+  Circom: [".circom"],
+  Clean: [".dcl", ".icl"],
+  Clojure: [
+    ".boot",
+    ".cl2",
+    ".clj",
+    ".cljs.hl",
+    ".cljscm",
+    ".cljx",
+    ".hic",
+    ".riemann.config",
+  ],
+  ClojureC: [".cljc"],
+  ClojureScript: [".cljs"],
+  CMake: [".cmake", ".cmake.in", ".CMakeLists.txt"],
+  COBOL: [".CBL", ".cbl", ".ccp", ".COB", ".cob", ".cobol", ".cpy"],
+  "CoCoA 5": [".c5", ".cocoa5", ".cocoa5server", ".cpkg5"],
+  CoffeeScript: [".coffee", ".cakefile", ".cjsx", ".iced"],
+  ColdFusion: [".cfm", ".cfml"],
+  "ColdFusion CFScript": [".cfc"],
+  "Constraint Grammar": [".cg3", ".rlx"],
+  Containerfile: [".Containerfile"],
+  Coq: [".v"],
+  Crystal: [".cr"],
+  CSON: [".cson"],
+  CSS: [".css"],
+  CSV: [".csv"],
+  Cucumber: [".feature"],
+  CUDA: [".cu", ".cuh"],
+  Cython: [".pxd", ".pxi", ".pyx"],
+  D: [".d"],
+  Dafny: [".dfy"],
+  DAL: [".da"],
+  Dart: [".dart"],
+  "Delphi Form": [".dfm"],
+  DenizenScript: [".dsc"],
+  Derw: [".derw"],
+  dhall: [".dhall"],
+  DIET: [".dt"],
+  diff: [".diff", ".patch"],
+  DITA: [".dita"],
+  Dockerfile: [".Dockerfile", ".dockerfile"],
+  "DOORS Extension Language": [".dxl"],
+  "DOS Batch": [".BAT", ".bat", ".BTM", ".btm", ".CMD", ".cmd"],
+  Drools: [".drl"],
+  DTD: [".dtd"],
+  dtrace: [".d"],
+  ECPP: [".ecpp"],
+  EEx: [".eex"],
+  EJS: [".ejs"],
+  Elixir: [".ex", ".exs"],
+  Elm: [".elm"],
+  "Embedded Crystal": [".ecr"],
+  ERB: [".ERB", ".erb"],
+  Erlang: [
+    ".app.src",
+    ".emakefile",
+    ".erl",
+    ".hrl",
+    ".rebar.config",
+    ".rebar.config.lock",
+    ".rebar.lock",
+    ".xrl",
+    ".yrl",
+  ],
+  Expect: [".exp"],
+  "F#": [".fsi", ".fs", ".fs"],
+  "F# Script": [".fsx"],
+  Fennel: [".fnl"],
+  "Finite State Language": [".fsl", ".jssm"],
+  "Fish Shell": [".fish"],
+  Flatbuffers: [".fbs"],
+  Focus: [".focexec"],
+  Forth: [
+    ".4th",
+    ".e4",
+    ".f83",
+    ".fb",
+    ".forth",
+    ".fpm",
+    ".fr",
+    ".frt",
+    ".ft",
+    ".fth",
+    ".rx",
+    ".fs",
+    ".f",
+    ".for",
+  ],
+  "Fortran 77": [
+    ".F",
+    ".F77",
+    ".f77",
+    ".FOR",
+    ".FTN",
+    ".ftn",
+    ".pfo",
+    ".f",
+    ".for",
+  ],
+  "Fortran 90": [".F90", ".f90"],
+  "Fortran 95": [".F95", ".f95"],
+  "Freemarker Template": [".ftl"],
+  Futhark: [".fut"],
+  FXML: [".fxml"],
+  GDScript: [".gd"],
+  "Gencat NLS": [".msg"],
+  Glade: [".glade", ".ui"],
+  Gleam: [".gleam"],
+  "Glimmer JavaScript": [".gjs"],
+  "Glimmer TypeScript": [".gts"],
+  GLSL: [
+    ".comp",
+    ".fp",
+    ".frag",
+    ".frg",
+    ".fsh",
+    ".fshader",
+    ".geo",
+    ".geom",
+    ".glsl",
+    ".glslv",
+    ".gshader",
+    ".tesc",
+    ".tese",
+    ".vert",
+    ".vrx",
+    ".vsh",
+    ".vshader",
+  ],
+  Go: [".go", ".ʕ◔ϖ◔ʔ"],
+  "Godot Resource": [".tres"],
+  "Godot Scene": [".tscn"],
+  "Godot Shaders": [".gdshader"],
+  Gradle: [".gradle", ".gradle.kts"],
+  Grails: [".gsp"],
+  GraphQL: [".gql", ".graphql", ".graphqls"],
+  Groovy: [".gant", ".groovy", ".grt", ".gtpl", ".gvy", ".jenkinsfile"],
+  Haml: [".haml", ".haml.deface"],
+  Handlebars: [".handlebars", ".hbs"],
+  Harbour: [".hb"],
+  Hare: [".ha"],
+  Haskell: [".hs", ".hsc", ".lhs"],
+  Haxe: [".hx", ".hxsl"],
+  HCL: [".hcl", ".nomad", ".tf", ".tfvars"],
+  HLSL: [".cg", ".cginc", ".fxh", ".hlsl", ".hlsli", ".shader"],
+  HolyC: [".HC"],
+  Hoon: [".hoon"],
+  HTML: [".htm", ".html", ".html.hl", ".xht"],
+  "HTML EEx": [".heex"],
+  IDL: [".dlm", ".idl", ".pro"],
+  Idris: [".idr"],
+  "Igor Pro": [".ipf"],
+  Imba: [".imba"],
+  INI: [".buildozer.spec", ".editorconfig", ".ini", ".lektorproject", ".prefs"],
+  InstallShield: [".ism"],
+  IPL: [".ipl"],
+  Jai: [".jai"],
+  Janet: [".janet"],
   Java: [".java"],
-  "C++": [".cpp", ".hpp", ".h"],
-  // 可以根据需要添加更多语言
+  JavaScript: [".js", ".jsx"],
+  "JavaServer Faces": [".jsf"],
+  JCL: [".jcl"],
+  "Jinja Template": [".j2", ".jinja", ".jinja2"],
+  JSON: [
+    ".arcconfig",
+    ".avsc",
+    ".composer.lock",
+    ".geojson",
+    ".gltf",
+    ".har",
+    ".htmlhintrc",
+    ".json",
+    ".json-tmlanguage",
+    ".jsonl",
+    ".mcmeta",
+    ".mcmod.info",
+    ".tern-config",
+    ".tern-project",
+    ".tfstate",
+    ".tfstate.backup",
+    ".topojson",
+    ".watchmanconfig",
+    ".webapp",
+    ".webmanifest",
+    ".yyp",
+  ],
+  JSON5: [".json5"],
+  JSP: [".jsp", ".jspf"],
+  JSX: [".jsx"],
+  Julia: [".jl"],
+  "Juniper Junos": [".junos"],
+  "Jupyter Notebook": [".ipynb"],
+  Kermit: [".ksc"],
+  "Korn Shell": [".ksh"],
+  Kotlin: [".kt", ".kts"],
+  "Kotlin Script": [".kts"],
+  "Ladder Logic": [".lad", ".ladder"],
+  Lasso: [".las"],
+  LCL: [".lcl"],
+  Lemon: [".y"],
+  LFE: [".lfe"],
+  LilyPond: [".ly"],
+  Liquid: [".liquid"],
+  Lisp: [".lisp", ".lsp", ".cl"],
+  LiveCode: [".livecode"],
+  Logtalk: [".lgt"],
+  LotusScript: [".lotusscript"],
+  Lua: [".lua"],
+  M4: [".m4"],
+  Makefile: [".makefile", ".mk", ".Makefile"],
+  Malbolge: [".malbolge"],
+  Markdown: [".markdown", ".md", ".mkd"],
+  MATLAB: [".m"],
+  MaxScript: [".ms"],
+  "Maya Embedded Language": [".mel"],
+  Mercury: [".m"],
+  Miranda: [".mira"],
+  Mojolicious: [".pl", ".pm"],
+  Monkey: [".monkey"],
+  Moonscript: [".moon"],
+  MQL4: [".mq4"],
+  MQL5: [".mq5"],
+  MSBuild: [".msbuild"],
+  MUMPS: [".mumps", ".m"],
+  NATURAL: [".ntr"],
+  Nim: [".nim"],
+  Ninja: [".ninja"],
+  Nix: [".nix"],
+  Nmap: [".nmap"],
+  OCaml: [".ml", ".mli"],
+  "OpenEdge ABL": [".p"],
+  OpenSCAD: [".scad"],
+  OpenType: [".otf"],
+  "Oracle PL/SQL": [".plsql", ".plb"],
+  Oz: [".oz"],
+  P: [".p"],
+  P4: [".p4"],
+  PASCAL: [".p", ".pas"],
+  "Pascal Script": [".pascal"],
+  "PascalABC.NET": [".pas"],
+  Perl: [".pl", ".pm"],
+  PGP: [".gpg"],
+  PHP: [".php", ".php3", ".php4", ".php5", ".phtml"],
+  Pike: [".pike"],
+  "PL/I": [".pli"],
+  "PL/SQL": [".plsql"],
+  PostScript: [".ps"],
+  PowerBuilder: [".pb"],
+  PowerShell: [".ps1", ".psd1", ".psm1"],
+  Processing: [".pde"],
+  Prolog: [".pl", ".pro"],
+  PromQL: [".promql"],
+  "Protocol Buffers": [".proto"],
+  PureScript: [".purs"],
+  Python: [".py", ".pyc", ".pyd", ".pyo"],
+  QML: [".qml"],
+  R: [".r", ".R"],
+  Raku: [".raku"],
+  Razor: [".razor"],
+  Reason: [".re"],
+  Rebol: [".r"],
+  Red: [".red"],
+  "Reinforcement Learning": [".rl"],
+  Remedy: [".remedy"],
+  RPG: [".rpg"],
+  Ruby: [".rb"],
+  Rust: [".rs"],
+  S: [".S", ".s"],
+  "S-Plus": [".s"],
+  SAS: [".sas"],
+  Sass: [".sass"],
+  Scala: [".scala"],
+  Scheme: [".scm", ".ss", ".sls"],
+  Scilab: [".sce"],
+  Scratch: [".sb", ".sb2", ".sb3"],
+  Sed: [".sed"],
+  Schematic: [".sch"],
+  Shell: [".sh"],
+  Shiny: [".R"],
+  Shor: [".shor"],
+  Simulink: [".slx"],
+  SML: [".sml"],
+  Solidity: [".sol"],
+  SourcePawn: [".sp", ".inc"],
+  SPICE: [".sp"],
+  SPSS: [".sav"],
+  SQL: [".sql"],
+  Squirrel: [".nut"],
+  SRecode: [".srecode"],
+  Stata: [".do", ".dta"],
+  Swift: [".swift"],
+  Tcl: [".tcl"],
+  Tidy: [".tidy"],
+  TypeScript: [".ts", ".tsx"],
+  VBScript: [".vbs"],
+  Verilog: [".v", ".sv"],
+  VHDL: [".vhd", ".vhdl"],
+  "Visual FoxPro": [".prg"],
+  "Visual Basic": [".bas", ".frm", ".cls"],
+  "Vue.js": [".vue"],
+  WebAssembly: [".wasm"],
+  Wolfram: [".w"],
+  "Wolfram Language": [".wl"],
+  "Wolfram Markup Language": [".wml"],
+  X11: [".x11"],
+  XQuery: [".xqy", ".xql"],
+  YAML: [".yaml", ".yml"],
+  Zig: [".zig"],
+  ZSH: [".zsh"],
 };
 
 interface CodeStats {
@@ -29,23 +415,26 @@ interface LanguageStats {
 export function activate(context: vscode.ExtensionContext) {
   console.log("Code Line Counter 扩展已激活");
 
-  let countCodeLinesDisposable = vscode.commands.registerCommand(
+  const countCodeLinesDisposable = vscode.commands.registerCommand(
     "codeLineCounter.countCodeLines",
     () => countCodeLines()
   );
 
-  let countCodeLinesInFolderDisposable = vscode.commands.registerCommand(
+  const countCodeLinesInFolderDisposable = vscode.commands.registerCommand(
     "codeLineCounter.countCodeLinesInFolder",
     (folderUri: vscode.Uri) => countCodeLines([folderUri.fsPath])
   );
 
-  context.subscriptions.push(countCodeLinesDisposable, countCodeLinesInFolderDisposable);
+  context.subscriptions.push(
+    countCodeLinesDisposable,
+    countCodeLinesInFolderDisposable
+  );
   console.log("countCodeLines 命令已注册");
 }
 
 async function countCodeLines(initialIncludeDirs: string[] = []) {
   console.log("countCodeLines 命令已执行");
-  
+
   // 获取当前工作区
   const workspaceFolders = vscode.workspace.workspaceFolders;
   if (!workspaceFolders) {
@@ -54,7 +443,7 @@ async function countCodeLines(initialIncludeDirs: string[] = []) {
   }
 
   const rootPath = workspaceFolders[0].uri.fsPath;
-  const defaultSrcPath = path.join(rootPath, 'src');
+  const defaultSrcPath = path.join(rootPath, "src");
 
   // 获取当前配置
   const config = vscode.workspace.getConfiguration("codeLineCounter");
@@ -81,16 +470,24 @@ async function countCodeLines(initialIncludeDirs: string[] = []) {
   });
 
   if (includeInput !== undefined) {
-    includeDirs = includeInput.split(",").map(dir => dir.trim());
+    includeDirs = includeInput.split(",").map((dir) => dir.trim());
   }
 
   if (excludeInput !== undefined) {
-    excludePatterns = excludeInput.split(",").map(pattern => pattern.trim());
+    excludePatterns = excludeInput.split(",").map((pattern) => pattern.trim());
   }
 
   // 更新配置
-  await config.update("includeDirs", includeDirs, vscode.ConfigurationTarget.Workspace);
-  await config.update("excludePatterns", excludePatterns, vscode.ConfigurationTarget.Workspace);
+  await config.update(
+    "includeDirs",
+    includeDirs,
+    vscode.ConfigurationTarget.Workspace
+  );
+  await config.update(
+    "excludePatterns",
+    excludePatterns,
+    vscode.ConfigurationTarget.Workspace
+  );
 
   const userDefinedLanguages =
     config.get<{ [key: string]: string[] }>("languageExtensions") || {};
@@ -170,19 +567,23 @@ function getLanguageFromExtension(
   return null;
 }
 
-function displayResults(stats: LanguageStats, includeDirs: string[], excludePatterns: string[]): void {
+function displayResults(
+  stats: LanguageStats,
+  includeDirs: string[],
+  excludePatterns: string[]
+): void {
   let message = "代码行统计结果:\n\n";
-  
+
   // 显示包含的文件夹
   message += "包含的文件夹:\n";
-  includeDirs.forEach(dir => {
+  includeDirs.forEach((dir) => {
     message += `  - ${dir}\n`;
   });
   message += "\n";
 
   // 显示排除的模式
   message += "排除的模式:\n";
-  excludePatterns.forEach(pattern => {
+  excludePatterns.forEach((pattern) => {
     message += `  - ${pattern}\n`;
   });
   message += "\n";
